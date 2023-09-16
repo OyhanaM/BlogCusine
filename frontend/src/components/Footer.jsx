4f09d7005bd89b90f99d3bbffa739f99dc11181f
OyhanaM <oyhana.mahjoubi@gmail.com>
2023-09-16 12:55:26 +0200

    create footer

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,7 +1,12 @@
 export default function Footer() {
   return (
-    <div>
-      <h2>Footer</h2>
+    <div className="bg-beige flex justify-between items-center px-10 font-montserrat font-light text-darkbrown">
+      <img
+        className="h-11"
+        src="/assets/logo/logo-text.png"
+        alt="Milkees baking club"
+      />
+      <span>Codé avec amour et bubble tea 🧋</span>
     </div>
   );
 }
